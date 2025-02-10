113c3f15d24cb5ecb20a0126c1e7d777f76f0f7b
rigellin73 <rigellin73@gmail.com>
2025-02-10 12:29:58 +0100

    setup canvas and bgr image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-from main_window import setup_main_window
+from main_window import setup_main_window, setup_main_canvas
 
 # Constants
 PINK = "#e2979c"
@@ -12,6 +12,12 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+BGR_IMAGE = "tomato.png"
+
 window = setup_main_window()
+bgr_image = tk.PhotoImage(file=BGR_IMAGE)
+
+canvas = setup_main_canvas(bgr_image)
+canvas.pack()
 
 window.mainloop()
--- main_window.py
+++ main_window.py
@@ -1,8 +1,15 @@
 import tkinter as tk
 
 W_TITLE = "Pomodoro"
+C_WIDTH = 200
+C_HEIGHT = 224
 
 def setup_main_window():
     main_window = tk.Tk()
     main_window.title(W_TITLE)
     return main_window
+
+def setup_main_canvas(bgr_image):
+    main_canvas = tk.Canvas(width=C_WIDTH, height=C_HEIGHT)
+    main_canvas.create_image(C_WIDTH / 2, C_HEIGHT / 2, image=bgr_image)
+    return main_canvas
