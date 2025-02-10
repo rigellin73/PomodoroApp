360be2f9872176c9b6baa31bd5eae350c303e475
rigellin73 <rigellin73@gmail.com>
2025-02-10 19:35:38 +0100

    add start timer action to display zeroes

--- timer.py
+++ timer.py
@@ -16,7 +16,8 @@ class Timer:
     def __init__(self, main_window):
         self.window = main_window
         self.reps = 0
-        self.timer = None
+        self.timer_action = None
+        self.start_timer_action = None
 
     def format_time(self, time_in_sec):
         minutes = int(time_in_sec / 60)
@@ -43,12 +44,15 @@ class Timer:
     def countdown(self, cur_time_sec):
         self.window.change_timer_text(self.format_time(cur_time_sec))
         if cur_time_sec > 0:
-            self.timer = self.window.delay_action(1000, self.countdown, cur_time_sec - 1)
+            self.timer_action = self.window.delay_action(1000, self.countdown, cur_time_sec - 1)
         else:
             if self.reps % 2 != 0:
                 self.window.add_checkmark()
-            self.start_timer()
+            self.start_timer_action = self.window.delay_action(1000, self.start_timer)
 
     def stop_timer(self):
-        self.window.cancel_action(self.timer)
+        if self.timer_action:
+            self.window.cancel_action(self.timer_action)
+        if self.start_timer_action:
+            self.window.cancel_action(self.start_timer_action)
         self.reps = 0
