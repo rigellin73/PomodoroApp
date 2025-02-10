92bbbcab7a4ae4bf1d6f17fa9fb51458cb85c318
rigellin73 <rigellin73@gmail.com>
2025-02-10 12:10:02 +0100

    setup main window

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+from main_window import setup_main_window
+
+# Constants
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+window = setup_main_window()
+
+window.mainloop()
---
+++ main_window.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+W_TITLE = "Pomodoro"
+
+def setup_main_window():
+    main_window = tk.Tk()
+    main_window.title(W_TITLE)
+    return main_window
